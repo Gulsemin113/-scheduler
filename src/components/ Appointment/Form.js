97a75430185b44e29d70162697327b871e4e7984
Gulsemin113 <gulseminyildiz@hotmail.com>
2021-12-09 20:56:48 -0500

    Resetting Inputs

--- src/components/ Appointment/Form.js
+++ src/components/ Appointment/Form.js
@@ -6,6 +6,22 @@ import InterviewerList from "components/InterviewerList";
 export default function Form(props) {
   const [name, setName] = useState(props.student || "");
   const [interviewer, setInterviewer] = useState(props.interviewer || null);
+
+  /*When the user clicks the Cancel button, we clear the form values. */
+  const reset = () => {
+    setName("");
+    setInterviewer(null);
+  };
+ 
+  /*
+    when the user clicks the cancel button
+    the form resets
+  */
+  const cancel = () => {
+    reset();
+    props.onCancel();
+  };
+
   return (
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
@@ -28,7 +44,7 @@ export default function Form(props) {
       </section>
       <section className="appointment__card-right">
         <section className="appointment__actions">
-          <Button danger onClick={props.onCancel}>
+          <Button danger onClick={cancel}>
             Cancel
           </Button>
           <Button confirm onClick={props.onSave}>
