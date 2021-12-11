994078c1ef0d1e7c5376bd0d5d55bdf0e1813039
Gulsemin113 <gulseminyildiz@hotmail.com>
2021-12-11 15:14:42 -0500

    Create a transition function within useVisualMode

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -2,6 +2,21 @@ import { useState } from "react";
 
 export default function useVisualMode(initial) {
   const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
 
-  return { mode };
+  const transition = (newMode, replace = false) => {
+    setMode(newMode);
+
+    setHistory((history) => {
+      if (replace) {
+        const newHistory = [...history];
+        newHistory.splice(-1, 1, newMode);
+        return newHistory;
+      } else {
+        return [...history, newMode];
+      }
+    });
+  };
+
+  return { mode, transition };
 }
