ce23302d5ba4782af8b791f9893485184a0f60fd
Gulsemin113 <gulseminyildiz@hotmail.com>
2021-12-10 21:50:56 -0500

    Create a new helpers directory and an empty selectors.js file within that.

---
+++ src/helpers/selectors.js
@@ -0,0 +1,8 @@
+/*
+  returns array of appointment objects for the current
+  selected day which will be used in Application.js
+  file
+*/
+export const getAppointmentsForDay = (state, day) => {
+  
+}
