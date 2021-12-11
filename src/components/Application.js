5b89c12986d651818b342e720c2ef4edc247d203
Gulsemin113 <gulseminyildiz@hotmail.com>
2021-12-11 00:10:45 -0500

    Managing State and Retrieving Appointments

--- src/components/Application.js
+++ src/components/Application.js
@@ -3,20 +3,29 @@ import "components/Application.scss";
 import DayList from "./DayList";
 import Appointment from "./ Appointment";
 import axios from "axios";
-
+import { getAppointmentsForDay } from "helpers/selectors";
 
 export default function Application(props) {
   const [state, setState] = useState({
-    
     day: "Monday",
     days: [],
-    appointments: {}
+    appointments: {},
   });
+  const setDay = (day) => setState({ ...state, day });
 
   useEffect(() => {
-    axios.get("/api/days")
-    .then(response => setDays(response.data));
-  }, [])
+    Promise.all([axios.get("/api/days"), axios.get("/api/appointments")]).then(
+      (all) => {
+        setState((state) => ({
+          ...state,
+          days: all[0].data,
+          appointments: all[1].data,
+        }));
+      }
+    );
+  }, []);
+
+  const appointmentsForDay = getAppointmentsForDay(state, state.day);
 
   return (
     <main className="layout">
@@ -28,7 +37,7 @@ export default function Application(props) {
         />
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
-          <DayList days={days} value={day} onChange={setDay} />
+          <DayList days={state.days} day={state.day} setDay={setDay} />
         </nav>
         <img
           className="sidebar__lhl sidebar--centered"
@@ -37,7 +46,7 @@ export default function Application(props) {
         />
       </section>
       <section className="schedule">
-        {appointments.map((appointment) => (
+        {appointmentsForDay.map((appointment) => (
           <Appointment key={appointment.id} {...appointment} />
         ))}
         <Appointment id="last" time="5pm" />
