d4455a19321951a6284bbf48c043668099c7c781
Gulsemin113 <gulseminyildiz@hotmail.com>
2021-12-11 00:50:14 -0500

    Create a function getInterview(state, interview) in src/helpers/selectors.js.

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -4,7 +4,7 @@
   file
 */
 export const getAppointmentsForDay = (state, day) => {
-  const dayObj = state.days.find(element => element.name === day);
+  const dayObj = state.days.find((element) => element.name === day);
 
   if (!dayObj) {
     return [];
@@ -14,11 +14,33 @@ export const getAppointmentsForDay = (state, day) => {
 
   const appointmentsForDay = [];
 
-  for(const id in state.appointments) {
+  for (const id in state.appointments) {
     if (appointmentIds.includes(Number(id))) {
-      appointmentsForDay.push(state.appointments[id])
+      appointmentsForDay.push(state.appointments[id]);
     }
   }
 
   return appointmentsForDay;
-}
+};
+
+/*
+  returns array of interview objects for the current
+  selected day which will be used in Application.js
+  file
+*/
+export const getInterview = (state, interview) => {
+  if (!interview) {
+    return null;
+  }
+
+  const interviewerId = interview.interviewer;
+
+  for (const id in state.interviewers) {
+    if (Number(id) === interviewerId) {
+      return {
+        student: interview.student,
+        interviewer: state.interviewers[id],
+      };
+    }
+  }
+};
