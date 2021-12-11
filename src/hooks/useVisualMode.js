af44c4b75f44b951bf24e29d5857384e44183186
Gulsemin113 <gulseminyildiz@hotmail.com>
2021-12-11 15:37:38 -0500

    Add a back() function to the Hook that allows us to go back to the previous mode.

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -16,13 +16,16 @@ export default function useVisualMode(initial) {
         return [...history, newMode];
       }
     });
-
-    
   };
 
   const back = () => {
-
-  }
+    setHistory((history) => {
+      const newHistory =
+        history.length > 1 ? [...history].slice(0, -1) : [...history];
+      setMode(newHistory[newHistory.length - 1]);
+      return newHistory;
+    });
+  };
 
   return { mode, transition, back };
 }
