001b4191f605d7a28d3fea17b77da50306b10643
Gulsemin113 <gulseminyildiz@hotmail.com>
2021-12-12 12:39:22 -0500

    Add a new constant for the CREATE mode similar to the ones for SHOW and EMPTY.

--- src/components/ Appointment/index.js
+++ src/components/ Appointment/index.js
@@ -3,19 +3,33 @@ import "./styles.scss";
 import Header from "./Header";
 import Show from "./Show";
 import Empty from "./Empty";
+import Form from "./Form";
 
+import useVisualMode from "hooks/useVisualMode";
+
+const EMPTY = "EMPTY";
+const SHOW = "SHOW";
+const CREATE = "CREATE";
 
 export default function Appointment(props) {
+  // custom hook to handle the visual modes of <Appointment> component
+  const { mode, transition, back } = useVisualMode(
+    props.interview ? SHOW : EMPTY
+  );
   return (
     <article className="appointment">
       <Header time={props.time} />
-      {props.interview ? (
+
+      {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
+
+      {mode === SHOW && (
         <Show
           student={props.interview.student}
           interviewer={props.interview.interviewer}
         />
-      ) : (
-        <Empty />
+      )}
+      {mode === CREATE && (
+        <Form interviewers={props.interviewers} onCancel={back} />
       )}
     </article>
   );
