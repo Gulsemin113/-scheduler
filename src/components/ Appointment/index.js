db44c6988e05efab9f4fc927fe7988916c0d0b85
Gulsemin113 <gulseminyildiz@hotmail.com>
2021-12-13 20:39:11 -0500

    Create a function called save in the Appointment component.

--- src/components/ Appointment/index.js
+++ src/components/ Appointment/index.js
@@ -10,12 +10,34 @@ import useVisualMode from "hooks/useVisualMode";
 const EMPTY = "EMPTY";
 const SHOW = "SHOW";
 const CREATE = "CREATE";
+const SAVE = "SAVE";
+const ERROR_SAVE = "ERROR_SAVE";
 
 export default function Appointment(props) {
   // custom hook to handle the visual modes of <Appointment> component
   const { mode, transition, back } = useVisualMode(
     props.interview ? SHOW : EMPTY
   );
+
+     /*
+    this function is passed as prop into the form
+    component and handles the visual mode to book
+    interview
+  */
+  function save(name, interviewer) {
+    const interview = {
+      student: name,
+      interviewer
+    };
+
+    transition(SAVE);
+
+    props
+      .bookInterview(props.id, interview)
+      .then(() => transition(SHOW))
+      .catch(() => transition(ERROR_SAVE, true));
+  }
+
   return (
     <article className="appointment">
       <Header time={props.time} />
