00491b53ce133563cd60573c9e0c6ee1a125cbc6
Gulsemin113 <gulseminyildiz@hotmail.com>
2021-12-15 19:22:25 -0500

    Change the Appointment folder name

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 import DayList from "components/DayList";
-import Appointment from "../components/ Appointment";
+import Appointment from "./Appointment";
 import {
   getAppointmentsForDay,
   getInterviewersForDay,
