2b8ea85417bbdd0b01dd9f45a872ee5e7e12a082
Gulsemin113 <gulseminyildiz@hotmail.com>
2021-12-15 19:09:28 -0500

    Change the Appointment folder name and add validate function

---
+++ src/components/Appointment/Form.js
@@ -0,0 +1,76 @@
+import React, { useState } from "react";
+import "./styles.scss";
+import Button from "components/Button";
+import InterviewerList from "components/InterviewerList";
+
+export default function Form(props) {
+  const [name, setName] = useState(props.name || "");
+  const [interviewer, setInterviewer] = useState(props.interviewer || null);
+  const [error, setError] = useState("");
+
+  /*
+    when the user cancels out an appointment form,
+    clears the selection and name input
+  */
+  const reset = () => {
+    setName("");
+    setInterviewer(null);
+  };
+
+  /*
+    when the user clicks the cancel button
+    the form resets
+  */
+  const cancel = () => {
+    reset();
+    props.onCancel();
+  };
+
+  /*
+    when the user clicks the save button
+    user entered a name into form is validated
+  */
+  function validate() {
+    if (name === "") {
+      setError("Student name cannot be blank");
+      return;
+    }
+
+    setError("");
+    props.onSave(name, interviewer);
+  }
+
+  return (
+    <main className="appointment__card appointment__card--create">
+      <section className="appointment__card-left">
+        <form autoComplete="off" onSubmit={(event) => event.preventDefault()}>
+          <input
+            className="appointment__create-input text--semi-bold"
+            name="name"
+            type="text"
+            placeholder="Enter Student Name"
+            value={name}
+            onChange={(event) => setName(event.target.value)}
+            data-testid="student-name-input"
+          />
+        </form>
+        <section className="appointment__validation">{error}</section>
+        <InterviewerList
+          interviewers={props.interviewers}
+          value={interviewer}
+          onChange={setInterviewer}
+        />
+      </section>
+      <section className="appointment__card-right">
+        <section className="appointment__actions">
+          <Button danger onClick={cancel}>
+            Cancel
+          </Button>
+          <Button confirm onClick={validate}>
+            Save
+          </Button>
+        </section>
+      </section>
+    </main>
+  );
+}
